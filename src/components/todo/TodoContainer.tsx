67d0951f886f7a729da9a8a37a046b668ac7880e
FuadTalukder85 <webfuadtk@gmail.com>
2024-02-10 01:06:30 +0600

    first commit

---
+++ src/components/todo/TodoContainer.tsx
@@ -0,0 +1,34 @@
+import { useAppSelector } from "@/redux/hook";
+import AddTodoModal from "./AddTodoModal";
+import TodoCard from "./TodoCard";
+import TodoFilter from "./TodoFilter";
+import { useGetTodoQuery } from "@/redux/api/api";
+
+const TodoContainer = () => {
+  // from local state
+  // const { todos } = useAppSelector((state) => state.todos);
+
+  // from server
+  const { data: todos, isLoading, isError } = useGetTodoQuery(undefined);
+  console.log(todos);
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  return (
+    <div>
+      <div className="flex justify-between mb-5">
+        <AddTodoModal></AddTodoModal>
+        <TodoFilter></TodoFilter>
+      </div>
+      <div className="bg-primary-gradient w-full h-full rounded-xl p-[5px]">
+        <div className="bg-white p-5 w-full h-full rounded-lg space-y-3">
+          {todos?.data?.map((item) => (
+            <TodoCard {...item}></TodoCard>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TodoContainer;
